ef22f10c726aee14c74d814f838a57cc280aa045
Faith <faithkanini@gmail.com>
2022-09-14 08:37:40 -0700

    Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 /**
  * print_last_digit - function prints the last digit of a number.
  * @ld: last digit result of a number.
- * Result: value of last digit.
+ * Return: value of last digit.
  */
 int print_last_digit(int ld)
 {
