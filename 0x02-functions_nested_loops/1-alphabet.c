3b418117b3b6b62a9c927c9280f36473e09fb369
Faith <faithkanini@gmail.com>
2022-09-14 05:17:01 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always zero.
+ */
+void print_alphabet(void)
+{
+	int n;
+	for (n ='a'; n <='z'; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
