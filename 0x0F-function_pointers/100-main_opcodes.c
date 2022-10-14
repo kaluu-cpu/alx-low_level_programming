bd12bd03ec0805daa407310c96a80694fa8c17c4
kaluu-cpu <faithkanini97@gmail.com>
2022-10-14 06:35:55 -0700

    Advanced Task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - the starting function. Receives an argument.
+ * @argc: the number of arguments + 1
+ * @argv: an array of passed arguments including program's name
+ *
+ * Return: 0 on Success, else 1 or 2 depending on the failed condition
+ */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int n_bytes, i;
+	char *arr_code;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		return (1);
 	}
 
+	n_bytes = atoi(argv[1]);
+	if (n_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	arr_code = (char *)main;
+
+	for (i = 0; i < n_bytes; i++)
+	{
+		if (i == n_bytes - 1)
+		{
+			printf("%02hhx\n", arr_code[i]);
+			break;
+		}
+		printf("%02hhx ", arr_code[i]);
+	}
 	return (0);
 }
 
